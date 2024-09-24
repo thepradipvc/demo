61cdcf175cb391f442bbd476f6f180070b02a8c0
Rohit <--add>
2024-09-24 21:20:31 +0530

    Template add (7)

--- src/app/templates/template7/page.tsx
+++ src/app/templates/template7/page.tsx
@@ -1,35 +1,141 @@
-"use client";
 import React from "react";
-import { BackgroundBeams } from "@/components/ui-bg/bg-beams";
-import { BackgroundBeamsWithCollision } from "@/components/ui-bg/bg-beams-collision";
-import StarsBG from '../../../components/ui-bg/stars-bg/index'
+import styles from "./styles.module.scss";
+import { profileData } from "../../templates/template1/ProfileData";
+import {
+  IconBrandDribbble,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandPinterest,
+  IconBrandSnapchat,
+  IconBrandSpotify,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+import Image from "next/image";
+import Card from "@/components/Card";
+import FormCard from "@/components/FormCard";
+import RockBG from '../../../components/ui-bg/rock-texture/index'
 
-function BackgroundBeamsDemo() {
+function TemplateFour() {
+  const { profile } = profileData;
+
+  const social_links = profile?.social_links;
+
+  const cardClasses = {
+    cardWrapper: styles.card__wrapper,
+    topDiv: styles.top_div,
+    imgWrap: styles.img_wrap,
+    headingWrap: styles.heading_wrap,
+    bottomDiv: styles.bottom_div,
+    button: styles.button,
+    price: styles.price,
+  };
+
+  const formClasses = {
+    cardWrapper: styles.form__wrapper,
+    topDiv: styles.form_top_div,
+    imgWrap: styles.img_wrap,
+    headingWrap: styles.heading_wrap,
+    middleDiv: styles.form_middle_div,
+    bottomDiv: styles.form_bottom_div,
+    button: styles.button,
+    price: styles.price,
+  };
+
   return (
-    // <BackgroundBeamsWithCollision>
-    //   <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-    //     <div className="max-w-2xl mx-auto p-4">
-    //       <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-    //         Join the waitlist
-    //       </h1>
-    //       <p></p>
-    //       <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-    //         Welcome to MailJet, the best transactional email service on the web.
-    //         We provide reliable, scalable, and customizable email solutions for
-    //         your business. Whether you&apos;re sending order confirmations,
-    //         password reset emails, or promotional campaigns, MailJet has got you
-    //         covered.
-    //       </p>
-    //       <input
-    //         type="text"
-    //         placeholder="hi@manuarora.in"
-    //         className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-    //       />
-    //     </div>
-    //   </div>
-    // </BackgroundBeamsWithCollision>
-    <StarsBG/>
+    <>
+    <div className={styles.template__wrapper}>
+      <div className={styles.template__content__wrapper}>
+        {/* Profile picture Div */}
+        <div className={styles.profile__pic__wrapper}>
+          <div id="user-image">
+            <Image
+              src={profile?.profile_picture}
+              alt=" profile"
+              fill
+              objectFit="cover"
+            />
+          </div>
+        </div>
+
+        {/* User Bio Section */}
+        <div id="bio" className={styles.bio__wrapper}>
+          <h1>{profile?.name}</h1>
+          <p>{profile?.bio}</p>
+        </div>
+
+        {/* Social Links */}
+        <div
+          id="social-links-wrapper"
+          className={styles.social__links__wrapper}
+        >
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandPinterest />
+            </span>
+          </div>
+
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandSnapchat />
+            </span>
+          </div>
+
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandInstagram />
+            </span>
+          </div>
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandSnapchat />
+            </span>
+          </div>
+
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandInstagram />
+            </span>
+          </div>
+
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandLinkedin />
+            </span>
+          </div>
+
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandYoutube />
+            </span>
+          </div>
+
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandDribbble />
+            </span>
+          </div>
+
+          <div className={styles.social__handle}>
+            <span>
+              <IconBrandSpotify />
+            </span>
+          </div>
+        </div>
+
+        {/* // Creator Offerings // */}
+        <div className={styles.offerings_wrapper}>
+          {/* Book 1:1 Call */}
+
+          <Card classes={cardClasses} />
+          <FormCard formClasses={formClasses} />
+          <Card classes={cardClasses} />
+          <FormCard formClasses={formClasses} showNumber={true} />
+        </div>
+      </div>
+    </div>
+    <RockBG/>
+    </>
   );
 }
 
-export default BackgroundBeamsDemo;
+export default TemplateFour;
---
+++ src/components/ui-bg/rock-texture/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+function index() {
+  return <article className={styles.wrapper}></article>;
+}
+
+export default index;
