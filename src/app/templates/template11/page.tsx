d497863374358e6aaed229b84d87abe960299860
Rohit <--add>
2024-09-28 23:05:55 +0530

    Templates added

---
+++ src/app/templates/template11/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import LinesBG from '../../../components/ui-bg/lines-bg/index'
+
+function Template11() {
+  return (
+    <div>
+      <h1>
+        TEST
+      </h1>
+        <LinesBG/>
+    </div>
+  )
+}
+
+export default Template11
