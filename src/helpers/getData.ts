b2a5bafd833aa6cc4dd816c038ea3af897036899
Pradip Chaudhary <88336529+thepradipvc@users.noreply.github.com>
2024-10-16 22:25:52 +0530

    Update getData.ts

--- src/helpers/getData.ts
+++ src/helpers/getData.ts
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 export const getData = async (profile: string) => {
   const res = await fetch(`https://dev.lnkr.store/api/v1/profile/${profile}`, {
     // Revalidate cache for fresh data in case of ISR/SSG
